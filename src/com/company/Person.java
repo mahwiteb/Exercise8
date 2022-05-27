bf3bec3ca1552fb55a3003b8d6ff68da2c8bc88c
BlessingM <100141200+mahwiteb@users.noreply.github.com>
2022-05-27 14:35:33 +0200

    Add files via upload

---
+++ src/com/company/Person.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Person {
+package com.company;
+
+import java.util.Scanner;
+
+    public class Person {
+
+
+        Scanner sr = new Scanner(System.in);
+        String Mahwite , Blesing, street , city;
+        int zipCode;
+
+        void Initialize(){
+            System.out.println("Please enter your Name: ");
+            firstNamerobert56850 = sr.nextLine();
+            System.out.println("Please enter your Surname: ");
+            surnamemuchie56850 = sr.nextLine();
+            System.out.println("Please enter your Street name: ");
+            street56850 = sr.nextLine();
+            System.out.println("Please enter your City: ");
+            city56850 = sr.nextLine();
+            System.out.println("Please enter your Zip Code: ");
+            zipCode56850 = sr.nextInt();
+        }
+
+        void printData(){
+            System.out.println("Name: "+ Blessing);
+            System.out.println("Surname: "+ surnameMahwite);
+            System.out.println("Street Name: "+ street;
+            System.out.println("City: "+ city;
+            System.out.println("Zip Code: "+ zip);
+        }
+
+    }
+
